2962e0b23f59d19269a2c31daa213fb0e3cda397
hnwj <2948662591@qq.com>
2023-05-13 10:02:16 +0800

    完成了静态资源导入

--- .history/src/main_20230513095616.js
+++ .history/src/main_20230513095616.js
@@ -2,13 +2,15 @@ import './assets/main.css'
 
 import { createApp } from 'vue'
 import { createPinia } from 'pinia'
-
+import { getCategory } from '@/api/testAPI.js'
 
 import App from './App.vue'
 import router from './router'
 import '@/styles/common.scss'
 const app = createApp(App)
-
+getCategory().then(res => {
+  console.log(res)
+})
 app.use(createPinia())
 app.use(router)
 
